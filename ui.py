8d9661bd705cb3d9a1e879e4259ae08ab33a2aba
Aman Prajapati <amanprajapati22@gmail.com>
2026-05-13 13:46:10 +0530

    Streaming added in chatbot

--- ui.py
+++ ui.py
@@ -32,10 +32,16 @@ if user_input:
     with st.chat_message("user"):
         st.markdown(user_input)
 
-    response = chatbot.invoke({'messages': HumanMessage(content = user_input)}, config = CONFIG)
-    ai_message = response['messages'][-1].content
-    st.session_state['message_history'].append({'role': 'ai', 'content': ai_message})
-    with st.chat_message("ai"):
-        st.markdown(ai_message)
+    with st.chat_message("assistant"):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': HumanMessage(content = user_input)},
+                config = CONFIG,
+                stream_mode = 'messages'
+                )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
         
 
