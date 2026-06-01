f7099caec00b5434ff4c7b2724390576bd2849ca
Aman Prajapati <amanprajapati22@gmail.com>
2026-06-01 12:09:31 +0530

    SqLite DB added for memory storage with chatbot

--- backend.py
+++ backend.py
@@ -2,9 +2,10 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, BaseMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import StateGraph, START, END
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from typing import TypedDict, List, Annotated
 from langgraph.graph.message import add_messages
+import sqlite3
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -24,6 +25,9 @@ def chat_node(state: ChatState):
     response = llm.invoke(message)
     return {'messages': [response]}
 
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+# checkpointer
+checkpointer = SqliteSaver(conn=conn)
 
 # Defining the graph
 graph = StateGraph(ChatState)
@@ -32,6 +36,12 @@ graph.add_node('chat_node', chat_node)
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
-checkpointer = InMemorySaver()
-
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# to retrieve all thread
+def retrieve_all_thread():
+    all_thread = set()
+    for checkpoint in checkpointer.list(None):
+        all_thread.add(checkpoint.config['configurable']['thread_id'])
+        
+    return list(all_thread)
